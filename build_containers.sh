52df6a50a701fe9f77165d6cb15c76d4f3d5cdfd
Michael Turek <mjturek@linux.vnet.ibm.com>
2018-06-27 16:29:56 +0000

    Run tripleo container builds with duffy nodes
    Leveraging test-day-tools as a way to try using duffy nodes to
    build tripleo containers.
    
    Change-Id: Id6959778c64e4b787f51ca123c406d976a805cf6

---
+++ build_containers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install dependencies
+sudo yum -y install git ansible
+
+git clone https://github.com/rdo-infra/ci-config
+
+pushd ci-config
+
+    # Turn off container publishing for this experiment
+    sed -i 's/kolla_push: true/kolla_push: false/g' ci-scripts/tripleo-upstream/build-containers-images.sh
+
+    # Set kolla to build with ppc64le experimental kolla config
+    #sed -i "9i sed -i \"s/--config-file={{ kolla_tmpdir }}\\\/kolla-build.conf/--config-file=\\\/tmp\\\/kolla-build-ppc64le.conf/g\" rdo-kolla-build/tasks/main.yml" ci-scripts/tripleo-upstream/build-containers-images.sh 
+
+    # Run the container build
+    export RELEASE="master"
+    bash -xe ci-scripts/tripleo-upstream/build-containers-images.sh
+popd
