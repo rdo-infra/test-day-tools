b107a9aa98cab5c2b67aae50d6816567c045bcc2
David Moreau Simard <dmsimard@redhat.com>
2018-06-27 14:35:36 -0400

    Test
    Change-Id: Iec74f5b29ec48de23c8869de4b9cdec7713ffb7d

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+WORKSPACE="${WORKSPACE:-/tmp}"
+VENV="${WORKSPACE}/venv"
+
+[[ ! -d "${VENV}" ]] && virtualenv "${VENV}"
+source "${VENV}/bin/activate"
+pip install ansible
+
+# cico-get-node requests a duffy node and generates an ansible-compatible
+# inventory at $WORKSPACE/hosts
+ansible-playbook -i "${WORKSPACE}/hosts" playbook.yml
