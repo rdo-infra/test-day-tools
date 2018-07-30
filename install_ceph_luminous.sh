aac3a85305c23e6904a43aa8326ebb0198fb2a7b
Michael Turek <mjturek@linux.vnet.ibm.com>
2018-07-30 16:37:48 +0000

    Test ceph-luminous-release install
    Tests newly built ceph-luminous release package
    
    Change-Id: I54ba947159c1fdb72292cec5e68d2c8358d232b5

---
+++ install_ceph_luminous.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CEPH_LUMINOUS_RELEASE_URL=http://cbs.centos.org/kojifiles/packages/centos-release-ceph-luminous/1.1/1.el7.centos/noarch/centos-release-ceph-luminous-1.1-1.el7.centos.noarch.rpm
+
+#Add the repo
+sudo yum install $CEPH_LUMINOUS_RELEASE_URL
+
+
+
+
